b7e441fa393ff76a5e3018f275d11f61b5bf0f71
Ihor Blyzniuk <ihor.blyzniuk2017@gmail.com>
2021-02-20 12:58:26 +0200

    fixed burger menu, now onClick link, remove active. Add request to form

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      menuLink = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -10,11 +11,65 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+menuLink.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
 const counters = document.querySelectorAll('.use__rating-counter'),
       lines = document.querySelectorAll('.use__rating-line span');
 
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+const forms = document.querySelectorAll('form'),
+      message = {
+          loading: 'Загрузка...',
+          success: 'Спасибо! Скоро я с вами свяжусь',
+          failure: 'Что-то пошло не так...'
+      };
+
+forms.forEach(item => {
+    postData(item);
+});
+
+function postData(form){
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let statusMessage = document.createElement('div');
+        statusMessage.textContent = message.loading;
+        form.appendChild(statusMessage);
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        const formData = new FormData(form);
+        
+        const object = {};
+        formData.forEach(function(value, key){
+            object[key] = value;
+        });
+        const json = JSON.stringify(object);
+
+        request.send(json);
+        console.log(json);
+
+        request.addEventListener('load', () => {
+            if(request.status === 200){
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+                form.reset();
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 3000);
+            } else{
+                statusMessage.textContent = message.failure;
+            }
+        });
+    });
+}
 
 new WOW().init();
